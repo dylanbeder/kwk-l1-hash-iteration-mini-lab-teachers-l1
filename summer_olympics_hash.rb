40dde651d3a5644a9b2b73cb5cb82a33262b3be8
dylanbeder <dylan.beder@duke.edu>
2018-06-20 19:20:56 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,13 +1,13 @@
 
 def create_olympics_hash
   summer_olympics={:Sydney => "2000" , :Athens => "2004", :Beijing => "2008", :London => "2012"}
+  puts summer_olympics
 end
 
 def add_a_key_value_pair
-  create_olympics_hash
   summer_olympics[:Atlanta]="1996"
+  puts summer_olympics
 end
-puts summer_olympics
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
