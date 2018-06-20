4ba7b28e90973e004f57f3e0691047cf1b97c578
dylanbeder <dylan.beder@duke.edu>
2018-06-20 19:21:18 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,7 +1,7 @@
 
 def create_olympics_hash
   summer_olympics={:Sydney => "2000" , :Athens => "2004", :Beijing => "2008", :London => "2012"}
-  puts summer_olympics
+
 end
 
 def add_a_key_value_pair
